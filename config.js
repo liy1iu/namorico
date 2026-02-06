3ee5ebcb11121203f42b7db53f5796f38321d5ec
Art <71746623+liy1iu@users.noreply.github.com>
2026-02-06 09:36:46 -0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "você por acaso quer namorar comigo?"",
+    pageTitle: "você por acaso quer namorar comigo?",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
