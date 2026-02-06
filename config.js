10e61c4ee94fc9d7ec98663d5b5fd1d2d9210ba1
Art <71746623+liy1iu@users.noreply.github.com>
2026-02-06 10:44:08 -0300

    Update config.js

--- config.js
+++ config.js
@@ -57,12 +57,12 @@ const CONFIG = {
     },
 
     music: {
-        enabled: true,
-        autoplay: true,
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3",
-        startText: "musiquinha",
-        stopText: "para a musica!!!!!!!",
-        volume: 0.5
+        enabled: true,                     // Music feature is enabled
+        autoplay: true,                    // Try to autoplay (note: some browsers may block this)
+        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        startText: "🎵 Play Music",        // Button text to start music
+        stopText: "🔇 Stop Music",         // Button text to stop music
+        volume: 0.5                        // Volume level (0.0 to 1.0)
     }
 };
 
